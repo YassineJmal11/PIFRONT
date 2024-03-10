e5f0c80d9c43b13bead0edb9f7b274e907902f79
YassineJmal11 <yassine.jmal@esprit.tn>
2024-03-10 19:31:56 +0100

    hhhh

---
+++ src/app/Frontend/alltemplate/alltemplate.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alltemplate',
+  templateUrl: './alltemplate.component.html',
+  styleUrls: ['./alltemplate.component.css']
+})
+export class AlltemplateComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { HeaderComponent } from './Frontend/header/header.component';
+import { FooterComponent } from './Frontend/footer/footer.component';
+import { AlltemplateComponent } from './Frontend/alltemplate/alltemplate.component';
+import { ContactUsComponent } from './Frontend/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AlltemplateComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
